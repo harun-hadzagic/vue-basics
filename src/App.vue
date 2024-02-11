facc347c9616fb10ab2ad8d4c2dbb7a2fb0f896f
Harun Hadzagic <107354156+harun-hadzagic@users.noreply.github.com>
2024-02-11 23:12:47 +0100

    Adding first component

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul>
+    <learning-resource v-for="resource in storedResources" :key="resource.id" :title="resource.title" :description="resource.descriptio" :link="resource.link"></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
+export default {
+    components:{
+        LearningResource
+    },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: "official-guide",
+          title: "Official Guide!",
+          descriptio: "The official Vue.js documentation",
+          link: "https://vuejs.org",
+        },
+        {
+          id: " google",
+          title: "Google",
+          descriptio: "You need to learn how to Google!",
+          link: "https://google.com",
+        },
+      ],
+    };
+  },
+};
+</script>
