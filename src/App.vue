e41f45f5fa163243d7965a295900664635e5c545
Harun Hadzagic <107354156+harun-hadzagic@users.noreply.github.com>
2024-02-12 11:52:29 +0100

    Adding base components

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-<stored-resources :resources="storedResources"></stored-resources>
+    <the-header title="RememberMe"></the-header>
 </template>
 
 <script>
-import StoredResources from './components/learning-resources/StoredResources.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
 export default {
     components:{
-        StoredResources
+        TheHeader
     },
   data() {
     return {
--- src/components/learning-resources/LearningResource.vue
+++ src/components/learning-resources/LearningResource.vue
@@ -1,27 +1,27 @@
 <template>
-    <li>
-        <div>
-            <header>
-                <h3>
-{{ title }}
-                </h3>
-                <button>Delete</button>
-            </header>
-        </div>
-        <p>{{ description }}</p>
-        <nav>
-            <a :href="link">View Resource</a>
-        </nav>
-    </li>
+  <li>
+    <base-card>
+      <header>
+        <h3>
+          {{ title }}
+        </h3>
+        <base-button mode="flat">Delete</base-button>
+      </header>
+      <p>{{ description }}</p>
+      <nav>
+        <a :href="link">View Resource</a>
+      </nav>
+    </base-card>
+  </li>
 </template>
 
 <script>
+import BaseButton from "../UI/BaseButton.vue";
 export default {
-    props: ["title", "description", "link"],
-    data() {
-        
-    },
-}
+  components: { BaseButton },
+  props: ["title", "description", "link"],
+  data() {},
+};
 </script>
 
 <style scoped>
